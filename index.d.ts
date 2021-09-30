688ca5aff4dbc7e72251281a520b593d1eb6cc4f
Aditya Wikardiyan <aditya@igloohome.co>
2021-10-01 06:23:59 +0800

    Fix package.json & index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,15 +3,20 @@
  * @param {*} packageName = NPM Package name, should be string
  * @param {*} version     = NPM Package version, should be string
  */
-export default function versionCheck(packageName: string, version?: string): Promise<{
-    status: string;
-    data: {
-        exists: boolean;
-        latestVersion: any;
-    };
-    message?: undefined;
-} | {
-    status: string;
-    message: any;
-    data?: undefined;
-}>;
+declare namespace versionCheck2 {
+  interface Return {
+    status: string,
+    data?: {
+      exists: boolean,
+      latestVersion: string
+    },
+    message?: any
+  }
+}
+
+declare const versionCheck2: {
+  (packageName: string, version: string): Promise<versionCheck2.Return>;
+  default: typeof versionCheck2;
+}
+
+export = versionCheck2;
